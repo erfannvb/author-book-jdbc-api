6824337c133cdb992a6f856c61437571876cd83c
Erfan Navab <erfannavab@gmail.com>
2024-01-22 13:34:22 +0330

    Initial commit

---
+++ src/main/java/nvb/dev/authorbookjdbcapi/AuthorBookJdbcApiApplication.java
@@ -0,0 +1,13 @@
+package nvb.dev.authorbookjdbcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorBookJdbcApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorBookJdbcApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nvb/dev/authorbookjdbcapi/AuthorBookJdbcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nvb.dev.authorbookjdbcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorBookJdbcApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
