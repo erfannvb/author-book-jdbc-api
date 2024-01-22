8e6b4fbdf5fcfbd096f85699a8f69dcac6551cd5
Erfan Navab <erfannavab@gmail.com>
2024-01-22 15:46:29 +0330

    added data for the book object

--- src/test/java/nvb/dev/authorbookjdbcapi/MotherObject.java
+++ src/test/java/nvb/dev/authorbookjdbcapi/MotherObject.java
@@ -1,6 +1,7 @@
 package nvb.dev.authorbookjdbcapi;
 
 import nvb.dev.authorbookjdbcapi.domain.Author;
+import nvb.dev.authorbookjdbcapi.domain.Book;
 
 public class MotherObject {
     public static Author createTestAuthor() {
@@ -8,6 +9,15 @@ public class MotherObject {
                 .id(1L)
                 .name("John Doe")
                 .age(35)
+                .build();
+    }
+
+    public static Book createTestBook() {
+        return Book.builder()
+                .id(1L)
+                .isbn("123-456")
+                .title("You")
+                .authorId(1L)
                 .build();
     }
 }
