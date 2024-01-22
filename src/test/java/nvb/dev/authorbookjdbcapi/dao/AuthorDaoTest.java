c55d37e0d360d08e1debd0152d2556dafb0193da
Erfan Navab <erfannavab@gmail.com>
2024-01-22 15:35:33 +0330

    wrote unit test for the rowMapper

--- src/test/java/nvb/dev/authorbookjdbcapi/dao/AuthorDaoTest.java
+++ src/test/java/nvb/dev/authorbookjdbcapi/dao/AuthorDaoTest.java
@@ -10,15 +10,23 @@ import org.mockito.Mock;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import static nvb.dev.authorbookjdbcapi.MotherObject.createTestAuthor;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 class AuthorDaoTest {
 
     @Mock
     JdbcTemplate jdbcTemplate;
+
+    @Mock
+    ResultSet resultSet;
 
     @InjectMocks
     AuthorDaoImpl authorDao;
@@ -82,6 +90,21 @@ class AuthorDaoTest {
                 eq("DELETE FROM tbl_author WHERE id = ?"),
                 eq(1L)
         );
+    }
+
+    @Test
+    void testRowMapper() throws SQLException {
+        when(resultSet.getLong("id")).thenReturn(1L);
+        when(resultSet.getString("name")).thenReturn("John Doe");
+        when(resultSet.getInt("age")).thenReturn(35);
+
+        AuthorDaoImpl.AuthorRowMapper authorRowMapper = new AuthorDaoImpl.AuthorRowMapper();
+
+        Author author = authorRowMapper.mapRow(resultSet, 1);
+
+        assertEquals(1L, author.getId());
+        assertEquals("John Doe", author.getName());
+        assertEquals(35, author.getAge());
     }
 
 }
