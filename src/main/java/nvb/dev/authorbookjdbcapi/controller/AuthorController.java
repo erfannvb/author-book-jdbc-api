553d77dd08991c70cb77eb15afe8cd31dd9783ac
Erfan Navab <erfannavab@gmail.com>
2024-01-22 19:34:10 +0330

    added producer and consumer to the mappings

--- src/main/java/nvb/dev/authorbookjdbcapi/controller/AuthorController.java
+++ src/main/java/nvb/dev/authorbookjdbcapi/controller/AuthorController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import nvb.dev.authorbookjdbcapi.domain.Author;
 import nvb.dev.authorbookjdbcapi.service.AuthorService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,23 +18,23 @@ public class AuthorController {
 
     private final AuthorService authorService;
 
-    @PostMapping("/author")
+    @PostMapping(value = "/author", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HttpStatus> saveAuthor(@RequestBody Author author) {
         authorService.saveAuthor(author);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping("/author/{authorId}")
+    @GetMapping(value = "/author/{authorId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Optional<Author>> findAuthorById(@PathVariable long authorId) {
         return new ResponseEntity<>(authorService.findAuthorById(authorId), HttpStatus.OK);
     }
 
-    @GetMapping("/author/all")
+    @GetMapping(value = "/author/all", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Author>> findAllAuthors() {
         return new ResponseEntity<>(authorService.findAllAuthors(), HttpStatus.OK);
     }
 
-    @PutMapping("/author/{authorId}")
+    @PutMapping(value = "/author/{authorId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HttpStatus> updateAuthor(@PathVariable long authorId, @RequestBody Author author) {
         authorService.updateAuthor(authorId, author);
         return new ResponseEntity<>(HttpStatus.OK);
